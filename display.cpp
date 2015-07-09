1f6207a7c8710b74cd1447693a5e4c04c1f7aa5d
Brian Jackson <axnjaxn@axnjaxn.com>
2015-07-09 11:07:48 -0400

    Adding hooks.

--- display.cpp
+++ display.cpp
@@ -2,26 +2,30 @@
 
 using namespace byteimage;
 
-void MyDisplay::handleEvent(SDL_Event event) {
+void MyDisplay::handleEvent(SDL_Event event) { 
   if (layout.widgets[0].widget == fractal) {
     if (event.type == SDL_KEYDOWN || event.type == SDL_KEYUP)
       fractal->handleKeyEvent(event);
   }
-  //TODO: Palette key events
+  else if (layout.widgets[0].widget == editor) {
+    if (event.type == SDL_KEYDOWN || event.type == SDL_KEYUP);
+    //TODO: Palette key events
+  }
 
   WidgetDisplay::handleEvent(event);
 }
 
 void MyDisplay::update() {
-  if (layout.widgets[0].widget == fractal) {
+  if (layout.widgets[0].widget == fractal)
     fractal->update();
-  }
+  //TODO: Palette per-frame function?
       
   WidgetDisplay::update();
 }
   
-MyDisplay::MyDisplay() : WidgetDisplay(600, 800, "NewMandel") {
+MyDisplay::MyDisplay() : WidgetDisplay(600, 800, "NewMandel"), font("res/FreeSans.ttf", 20) {
   fractal = new FractalRender(this, canvas.nr, canvas.nc);
+  editor = new Editor(this);
   openFractal();
 }
   
@@ -29,12 +33,32 @@ MyDisplay::~MyDisplay() {
   delete fractal;
 }
 
+//TODO: This group (don't forget to add OSD printing to update function)
+bool MyDisplay::getInt(const std::string& prompt, int& v) { }
+
+bool MyDisplay::getFloat(const std::string& prompt, float& v) { }
+
+bool MyDisplay::getDouble(const std::string& prompt, double& v) { }  
+
+bool MyDisplay::getString(const std::string& prompt, std::string& v) { }
+
+void MyDisplay::print(const std::string& str, ...) {
+  va_list args;
+  va_start(args, str);
+  char buf[1024];
+  vsprintf(buf, str.c_str(), args);
+  osd.print(str);
+}
+
 void MyDisplay::openFractal() {
   layout.clear();
   layout.attach(fractal, 0, 0, canvas.nc, canvas.nr, false);
 }
-  
-void MyDisplay::openPalette() { }//TODO
+
+void MyDisplay::openPalette() {
+  layout.clear();
+  layout.attach(editor, 0, 0, canvas.nc, canvas.nr, false);
+}
 
 int main(int argc, char* argv[]) {
   TextRenderer font("res/FreeSans.ttf", 20);
--- display.h
+++ display.h
@@ -2,18 +2,34 @@
 #define _BPJ_NEWMANDEL_DISPLAY_H
 
 #include "fractal.h"
+#include "editor.h"
 
 class MyDisplay : public WidgetDisplay {
 protected:
+  TextRenderer font;
+  OSD_Scanner scanner;
+  OSD_Printer osd;
+  
   void handleEvent(SDL_Event event);
   void update();
   
 public:
   FractalRender* fractal;
+  Editor* editor;
   
   MyDisplay();  
   ~MyDisplay();
 
+  //Hooks to OSD_Scanner
+  bool getInt(const std::string& prompt, int& v);
+  bool getFloat(const std::string& prompt, float& v);
+  bool getDouble(const std::string& prompt, double& v);  
+  bool getString(const std::string& prompt, std::string& v);
+
+  //Hooks to OSD_Printer
+  void print(const std::string& str, ...);
+
+  //Switching program modes
   void openFractal();  
   void openPalette();
 };
